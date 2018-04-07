73144ab803080cfe225cd91e50332daa0894ddbe
gmw-zjw <gaomingwei2017@163.com>
2018-04-07 10:10:25 +0800

    'commit:player'

--- src/containers/Player.js
+++ src/containers/Player.js
@@ -1,24 +1,22 @@
-import { connect } from "react-dom";
-import {showPlayer, changeSong} from "../redux/actions";
+import { connect } from 'react-redux'
+import { showPlayer, changeSong } from '../redux/actions'
+import Player from '../compontents/paly/Player'
 
-import Player from "../compontents/paly/Player";
-
-/* 映射Redux到全局state到组件的props上 */
+// 映射到Redux全局的stats到组件的props上
 const mapStateToProps = (state) => ({
-    showStatus: state.showStatus,
+    showPlayer: state.showPlayer,
     currentSong: state.song,
-    playSongs: state.songs
-});
+    playerSongs: state.songs
+})
 
-/* 映射dispatch到props上 */
+// 映射dispatch 到props上
 const mapDispatchToProps = (dispatch) => ({
     showMusicPlayer: (status) => {
-        dispatch(showPlayer(status));
+        dispatch(showPlayer(status))
     },
     changeCurrentSong: (song) => {
-        dispatch(changeSong(song));
+        dispatch(changeSong(song))
     }
-});
+})
 
-/* 将UI组件包装到容器组件上 */
 export default connect(mapStateToProps, mapDispatchToProps)(Player)
