1dbbb3183ee2d4662eb142b17045e25c9f7b9969
gmw-zjw <gaomingwei2017@163.com>
2018-04-07 11:22:14 +0800

    'commit:progress'

--- src/compontents/paly/Progress.js
+++ src/compontents/paly/Progress.js
@@ -1,101 +1,112 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import PropTypes from "prop-types"
+import React, { Component } from 'react'
+import PropType from 'prop-types'
+import ReactDOM from 'react-dom'
 
-import "./progress.styl"
+import './progress.styl'
 
-class Progress extends React.Component {
-	componentDidUpdate() {
-		//组件更新后重新获取进度条总宽度
-		if (!this.progressBarWidth) {
-			this.progressBarWidth = ReactDOM.findDOMNode(this.refs.progressBar).offsetWidth;
-		}
+
+class Progress extends Component {
+
+	constructor(props) {
+		super(props)
+
+
 	}
+
+	componentDidUpdate() {
+
+		// 组件更新后重新获取进度条总宽度
+		if (!this.progressBarWithd) {
+			this.progressBarWithd = ReactDOM.findDOMNode(this.refs.progressBar).offsetWidth
+		}
+
+	}
+
 	componentDidMount() {
+		// 获取元素DOM
 		let progressBarDOM = ReactDOM.findDOMNode(this.refs.progressBar);
 		let progressDOM = ReactDOM.findDOMNode(this.refs.progress);
 		let progressBtnDOM = ReactDOM.findDOMNode(this.refs.progressBtn);
-		this.progressBarWidth = progressBarDOM.offsetWidth;
+		this.progressBarWithd = progressBarDOM.offsetWidth;
 
-		
-		let {disableButton, disableDrag, onDragStart, onDrag, onDragEnd} = this.props;
+		let { disableButton, disableDrag, onDragStart, OnDrag, onDragEnd } = this.props;
 		if (disableButton !== true && disableDrag !== true) {
-			//触摸开始位置
+			// 触摸位置
 			let downX = 0;
-			//按钮left值
+			// 按钮left值
 			let buttonLeft = 0;
 
-			progressBtnDOM.addEventListener("touchstart", (e) => {
-				let touch = e.touches[0];
+			progressBtnDOM.addEventListener('touchstart', (e) => {
+				let touch = e.touch[0];
 				downX = touch.clientX;
 				buttonLeft = parseInt(touch.target.style.left, 10);
 
 				if (onDragStart) {
 					onDragStart();
 				}
-			});
-			progressBtnDOM.addEventListener("touchmove", (e) => {
+			})
+
+			progressBtnDOM.addEventListener('touchmove', (e) => {
+				// 阻止浏览器的默认行为
 				e.preventDefault();
 
-				let touch = e.touches[0];
-				let diffX = touch.clientX - downX;
-				
-				let btnLeft = buttonLeft + diffX;
-				if (btnLeft > progressBarDOM.offsetWidth) {
-					btnLeft = progressBarDOM.offsetWidth;
-				} else if (btnLeft < 0) {
-					btnLeft = 0;
-				}
-				//设置按钮left值
-				touch.target.style.left = btnLeft + "px";
-				//设置进度width值
-				progressDOM.style.width = btnLeft / this.progressBarWidth * 100 + "%";
+				let touch = e.touch[0];
+				let diffx = touch.clientX - downX;
 
-				if (onDrag) {
-					onDrag(btnLeft / this.progressBarWidth);
+				let btnLeft = buttonLeft + diffx;
+				if (btnLeft > progressBarDOM.offsetWidth) {
+					btnLeft = this.progressBarDOM.offsetWidth;
+				} else if (btnLeft < 0) {
+					buttonLeft = 0;
 				}
-			});
-			progressBtnDOM.addEventListener("touchend", (e) => {
+				// 设置按钮left值
+				touch.target.style.left = btnLeft + 'px';
+				// 设置进度width值
+				progressDOM.style.width = buttonLeft / this.progressBarWidth * 100 + "%";
+
+				if (OnDrag) {
+					OnDrag(btnLeft / this.progressBarWidth)
+				}
+			})
+
+			progressBarDOM.addEventListener('touchend', (e) => {
 				if (onDragEnd) {
 					onDragEnd();
 				}
-			});
+			})
 		}
 	}
+
 	render() {
-		//进度值：范围 0-1
-		let {progress, disableButton}  = this.props;
+		// 进度范围 0--1
+		let { progress, disableButton } = this.props
 		if (!progress) progress = 0;
 
-		//按钮left值
+		// 按钮 left 值
 		let progressButtonOffsetLeft = 0;
-		if(this.progressBarWidth){
-			progressButtonOffsetLeft = progress * this.progressBarWidth;
+		if (this.progressBarWithd) {
+			progressButtonOffsetLeft = progress * this.progressBarWithd
 		}
-
 		return (
 			<div className="progress-bar" ref="progressBar">
 				<div className="progress-load"></div>
-				<div className="progress" style={{width:`${progress * 100}%`}} ref="progress"></div>
+				<div className="progress" style={{ width: `${progress * 100}%` }} ref="progress"></div>
 				{
-					disableButton === true ? "" : 
-					<div className="progress-button" style={{left:progressButtonOffsetLeft}} ref="progressBtn"></div>
+					disableButton === true ? "" :
+						<div className="progress-button" style={{ left: progressButtonOffsetLeft }} ref="progressBtn"></div>
 				}
 			</div>
 		);
 	}
 }
 
-Progress.propTypes = {
-	//进度
-	progress: PropTypes.number.isRequired,
-	//是否禁用按钮
-	disableButton: PropTypes.bool,
-	//是否禁用拖拽
-	disableDrag: PropTypes.bool,
-	onDragStart: PropTypes.func,
-	onDrag: PropTypes.func,
-	onDragEnd: PropTypes.func
+Progress.PropType = {
+	progress: PropType.number.isRequired,
+	disableButton: PropType.bool,
+	disableDrag: PropType.bool,
+	onDragStart: PropType.func,
+	OnDrag: PropType.func,
+	onDragEnd: PropType.func
 };
 
 export default Progress
